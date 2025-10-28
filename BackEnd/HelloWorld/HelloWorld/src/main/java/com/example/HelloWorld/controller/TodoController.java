c3b7b478fc710c6b3105614c53bf38bd73712047
RiyaasathMJM <riyasathmjm@gmail.com>
2025-10-28 06:49:50 +0530

    TodoController Created

---
+++ BackEnd/HelloWorld/HelloWorld/src/main/java/com/example/HelloWorld/controller/TodoController.java
@@ -0,0 +1,98 @@
+package com.example.HelloWorld.controller;
+
+import com.example.HelloWorld.models.Todo;
+import com.example.HelloWorld.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping("api/v1/todo")
+
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping("/get1")
+    String getTodo() {
+        //  todoService.printTodo();
+        return "Todo";
+    }
+
+
+
+    //Path variable
+    /*@GetMapping("/{name}")
+    String getTodoById(@PathVariable String name) {
+        return "Todo with name: " + name;
+    }
+
+    // Request Param
+    @GetMapping("")
+    String getTodoByName(@RequestParam("toDoId") int  id) {
+        return "ID: " + id;
+    }
+*/
+    // Username & Password
+    @GetMapping("/create2")
+    String getDetails(@RequestParam String userId,String password){
+        return  "Username:" + userId+"  Password:"+password;
+    }
+    // For privacy purpose
+    @PostMapping("/create1")
+    String getDetails1(@RequestBody String body){
+        return body;
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Todo> updateTodoById(@PathVariable Integer id, @RequestBody Todo todo) {
+        todo.setId(id);
+        return new ResponseEntity<>(todoService.updateTodo(todo), HttpStatus.OK);
+
+    }
+
+    /* @DeleteMapping ("/{id}")
+     String DeleteToDoById(@PathVariable int id){
+         return "Delete with ID : " +id;
+     }
+ */
+//Create Todo
+    @PostMapping("/create")
+    ResponseEntity<Todo> createTodo(@RequestBody Todo todo){
+        return  new ResponseEntity<>(todoService.createTodo(todo),HttpStatus.CREATED);
+    }
+
+    // returning Todo by Id
+    @GetMapping("/{id}")
+    ResponseEntity<Todo> getTodoById(@PathVariable Integer id) {
+        return new ResponseEntity<>(todoService.getTodoById(id),HttpStatus.OK);
+    }
+
+    //returning  Todos in a list
+    @GetMapping("/get")
+    ResponseEntity<List<Todo>> getTodos(){
+        return new ResponseEntity<>(todoService.getTodos(),HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    ResponseEntity<Todo> updateTodoCon(@RequestBody Todo todo){
+        return  new ResponseEntity<>(todoService.updateTodo(todo),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping ("/{id}")
+    void DeleteToDoById(@PathVariable int id){
+
+        todoService.deleteTodoById(id);
+    };
+
+    @GetMapping("/page")
+    ResponseEntity<Page<Todo>> getTodosPaged(@RequestParam int page ,@RequestParam int size){
+        return new ResponseEntity<>(todoService.getAllTodoPages(page, size),HttpStatus.OK);
+    }
+
+}
